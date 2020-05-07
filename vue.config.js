fab92c74981997b1458ab9d86554122c621f323d
linchong <linchong@yunzhangfang.com>
2020-05-07 15:15:54 +0800

    去掉暂时用不到的功能

--- vue.config.js
+++ vue.config.js
@@ -1,75 +1,8 @@
-const path = require('path')
-const webpack = require('webpack')
-
-function resolve(dir) {
-  return path.join(__dirname, dir)
-}
-
-const isProd = process.env.NODE_ENV === 'production'
-
-const assetsCDN = {
-  // webpack build externals
-  externals: {
-    vue: 'Vue',
-    'vue-router': 'VueRouter',
-    vuex: 'Vuex',
-    axios: 'axios'
-  },
-  css: [],
-  // https://unpkg.com/browse/vue@2.6.10/
-  js: [
-    '//cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js',
-    '//cdn.jsdelivr.net/npm/vue-router@3.1.3/dist/vue-router.min.js',
-    '//cdn.jsdelivr.net/npm/vuex@3.1.1/dist/vuex.min.js',
-    '//cdn.jsdelivr.net/npm/axios@0.19.0/dist/axios.min.js'
-  ]
-}
-
 module.exports = {
   lintOnSave: true,
   publicPath: '/',
   outputDir: "dist",
   assetsDir: "static",
-  configureWebpack: {
-    // webpack plugins
-    plugins: [
-      // Ignore all locale files of moment.js
-      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
-    ],
-    // if prod, add externals
-    externals: isProd ? assetsCDN.externals : {}
-  },
-  chainWebpack: (config) => {
-    config.resolve.alias
-      .set('@$', resolve('src'))
-
-    const svgRule = config.module.rule('svg')
-    svgRule.uses.clear()
-    svgRule
-      .oneOf('inline')
-      .resourceQuery(/inline/)
-      .use('vue-svg-icon-loader')
-      .loader('vue-svg-icon-loader')
-      .end()
-      .end()
-      .oneOf('external')
-      .use('file-loader')
-      .loader('file-loader')
-      .options({
-        name: 'assets/[name].[hash:8].[ext]'
-      })
-    // 为文件添加hash值
-    config.output.filename('[name].[hash].js').end()
-    // if prod is on
-    // assets require on cdn
-    if (isProd) {
-      config.plugin('html').tap(args => {
-        args[0].cdn = assetsCDN
-        return args
-      })
-    }
-  },
-
   css: {
     loaderOptions: {
       postcss: {
